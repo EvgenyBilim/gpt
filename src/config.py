677506ad236f65ff915dcf914e14c62ef50afbfd
bilim <bilim-ea@zvuk.com>
2025-05-01 01:09:08 +0300

    commit9

--- src/config.py
+++ src/config.py
@@ -2,14 +2,14 @@
 
 vocab_size = 50257    # стандартное для GPT2
 block_size = 1024     # сколько токенов в одном куске текста
-n_layer = 24  # 12
-n_head = 16  # 12
-n_embd = 1024  # 768
+n_layer = 12  # 12
+n_head = 12  # 12
+n_embd = 768  # 768
 
 
 dropout = 0.1
 
-batch_size = 64  # 6
+batch_size = 32  # 6
 learning_rate = 3e-4
 max_iters = 50_000
 eval_interval = 1000
